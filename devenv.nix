9cffa8cea57b474c899f196cf7dffa342750fbfc
Lietze Diderich <lietze14@gmail.com>
2026-05-31 22:43:02 +0200

    chore: remove devenv configuration files and update README with project details

--- devenv.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, lib, config, inputs, ... }:
-{
-  dotenv.disableHint = true;
-  processes = {
-    serve.exec = "php artisan serve";
-    vite.exec = "npm run dev";
-    queue.exec = "php artisan queue:work";
-  };
-}
