b723efe665929d08602034ab2f509b9fb41a4bc1
YvonCrielaard <yvon.crielaard@gmail.com>
2026-06-05 11:30:43 +0200

    Styling aangepast

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,18 +1,21 @@
 <nav class="relative bg-olivegreen-500 border-t border-olivegreen-600">
-    <div class="max-w-4xl mx-auto px-6">
-        <div class="flex justify-between items-center h-14">
+    <div class="max-w-7xl mx-auto px-6">
+        <div class="relative flex justify-between items-center h-14">
 
-            <div class="flex items-center gap-6">
-                <img src="{{ asset('favicon.svg') }}" alt="De Groene Weide" class="h-8 w-8">
-                <a class="px-3 py-1.5 text-base text-white tracking-tight">
-                    Dit product is gemaakt door studenten aan Hogeschool Utrecht.
-                </a>
+            <div class="flex gap-4">
+                <img src="{{ asset('logo.svg') }}" alt="De Groene Weide" class="h-8 w-8">
+            </div>
 
-                <div class="hidden sm:flex items-center gap-3 text-sm">
-                    <a href="{{ route('home') }}" class="px-3 py-1.5 rounded-md text-white hover:bg-olivegreen-600 transition-colors">Over ons</a>
-                    <a href="{{ route('campsites.index') }}" class="px-3 py-1.5 rounded-md text-white hover:bg-olivegreen-600 transition-colors">Contact</a>
-                    <a href="{{ route('privacy') }}" class="px-3 py-1.5 rounded-md text-white hover:bg-olivegreen-600 transition-colors">Privacystatement</a>
-                </div>
+             <div class="absolute left-1/2 -translate-x-1/2 hidden sm:flex gap-3 text-sm">
+                <a href="{{ route('home') }}" class="px-3 py-1.5 text-white hover:underline">Over ons</a>
+                <a href="{{ route('campsites.index') }}" class="px-3 py-1.5 text-white hover:underline">Contact</a>
+                <a href="{{ route('privacy') }}" class="px-3 py-1.5 text-white hover:underline">Privacystatement</a>
+            </div>
+
+            <div class="text-right max-w-xs">
+                <span class="text-xs text-white tracking-tight">
+                    Dit product is gemaakt door studenten op Hogeschool Utrecht.
+                </span>
             </div>
 
         </div>
