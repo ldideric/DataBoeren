d3388deeb28b7ae852e85204ebcf905d3895412d
Hanna410 <hanna.s.nowak@gmail.com>
2026-06-15 10:04:20 +0200

    changes with the text about the activities

--- resources/views/pages/activities.blade.php
+++ resources/views/pages/activities.blade.php
@@ -111,32 +111,28 @@
                 </h2>
 
                 <div class="grid grid-cols-[1fr_320px] gap-8 items-center">
-                <p class="text-sm text-black mb-6">Supermarkt 4 kilometer.
-                    Voor dagelijkse boodschappen vind je op een korte afstand van de camping een supermarkt met een ruim assortiment aan producten.
+                <p class="text-sm text-black mb-6"> Voor dagelijkse boodschappen vind je op 4 afstand van de camping een supermarkt met een ruim assortiment aan producten.
                 </p>
                 <img src="/img/supermarkt.png"
                      onclick="openModel('/img/supermarkt.png')"
                      class="mx-auto w-80 rounded-lg shadow-md cursor-pointer hover:scale-105 transition">
                 </div>
                 <div class="grid grid-cols-[1fr_320px] gap-8 items-center">
-                    <p class="text-sm text-black mb-6">Markt 6 kilometer.
-                        Bezoek de lokale markt en ontdek verse producten uit de streek en leuke kraampjes. 
+                    <p class="text-sm text-black mb-6"> Bezoek de lokale markt op 6 kilometer van de camping en ontdek verse producten uit de streek en leuke kraampjes. 
                     </p>
                     <img src="/img/markt.png"
                      onclick="openModal('/img/markt.png')"
                      class="mx-auto w-80 rounded-lg shadow-md cursor-pointer hover:scale-105 transition">
                 </div>
                 <div class="grid grid-cols-[1fr_320px] gap-8 items-center">
-                     <p class="text-sm text-black mb-6">Zwembad 8 kilometer
-                     Liever zwemmen in een zwembad? Op slechts enkele kilometers van de camping bevindt zich een zwembad waar jong en oud zich kan vermaken.
+                     <p class="text-sm text-black mb-6"> Liever zwemmen in een zwembad? Op slechts 8 kilometers van de camping bevindt zich een zwembad waar jong en oud zich kan vermaken.
                      </p>
                      <img src="/img/zwembad.png"
                           onclick="openModel('/img/zwembad.png')"
                           class="mx-auto w-80 rounded-lg shadow-md cursor-pointer hover:scale-105 transition">
                 </div>
                 <div class="grid grid-cols-[1fr_320px] gap-8 items-center">
-                    <p class="text-sm text-black mb-6">Dichtstbijzijnde dorp 3 kilometer
-                        Het nabijgelegen dorp biedt verschillende voorzieningen, zoals winkels, café's, restaurants en een mooie kerk.
+                    <p class="text-sm text-black mb-6"> Het nabijgelegen dorp op 3 kilometer van de camping biedt verschillende voorzieningen, zoals winkels, café's, restaurants en een mooie kerk.
                     </p>
                     <img src="/img/dorp.png"
                      onclick="openModal('/img/dorp.png')"
