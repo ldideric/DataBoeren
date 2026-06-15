861f37113a99e3e5521306218d74ed9d4b79c7cb
Eidolonguy <bryan.s2424@gmail.com>
2026-06-15 15:59:04 +0200

    functionele icoontjes

--- resources/views/campsites/map/index.blade.php
+++ resources/views/campsites/map/index.blade.php
@@ -4,7 +4,6 @@
     @php
     $dbCampsites = \App\Models\Campsite::all()
         ->keyBy(fn($c) => $c->name . '|' . $c->type->value);
-
     $campsites = collect(json_decode(file_get_contents(database_path('src\campsites.json')), true))
         ->filter(fn($c) => isset($c['lat'], $c['lng']))
         ->map(fn($c) => array_merge($c, [
@@ -21,34 +20,48 @@
         <script>
         document.addEventListener('DOMContentLoaded', () => {
             const IMAGE_URL   = @json(asset('img/camping_map.png'));
-            const MAP_OPTIONS = { crs: L.CRS.Simple, minZoom: -2.6, maxZoom: -0.5, zoomControl: true };
-            const ICON        = L.icon({ iconUrl: 'img/marker_placeholder.png', iconSize: [38, 38], iconAnchor: [19, 19] });
+            const MAP_OPTIONS = { crs: L.CRS.Simple, minZoom: -2.6, maxZoom: -0.5, zoomControl: false };
+            const map = L.map('map', MAP_OPTIONS);
+            L.control.zoom({ position: 'topright' }).addTo(map);
+            
             const campsites   = @json($campsites);
 
-            const map = L.map('map', MAP_OPTIONS);
+            const ICONS = {
+                'Varkensveld': L.icon({ iconUrl: 'img/icon_varken.png', iconSize: [50, 50], iconAnchor: [19, 19] }),
+                'Paardenveld': L.icon({ iconUrl: 'img/icon_paard.png', iconSize: [42, 42], iconAnchor: [19, 19] }),
+            };
+            const DEFAULT_ICON = L.icon({ iconUrl: 'img/icon_default.png', iconSize: [42, 42], iconAnchor: [19, 19] });
 
             const groups = {};
+
             campsites.forEach(site => {
                 const type = site.type ?? 'Overig';
                 if (!groups[type]) groups[type] = L.layerGroup().addTo(map);
 
-                const marker = L.marker([site.lat, site.lng], { icon: ICON });
+                const icon   = ICONS[type] ?? DEFAULT_ICON;
+                const marker = L.marker([site.lat, site.lng], { icon });
+
                 marker.on('click', () => {
-                if (typeof openModal !== 'function') return;
-                const el = document.createElement('div');
-                el.dataset.campsiteId   = site.id             ?? '';
-                el.dataset.name         = site.name           ?? '';
-                el.dataset.campsiteType = site.type           ?? '';
-                el.dataset.people       = site.max_people     ?? '';
-                el.dataset.vehicles     = site.max_vehicles   ?? '';
-                el.dataset.electricity  = site.has_electricity ? 'true' : 'false';
-                el.dataset.notes        = site.notes          ?? '';
-                openModal(el);
-            });
+                    if (typeof openModal !== 'function') return;
+                    const el = document.createElement('div');
+                    el.dataset.campsiteId   = site.id              ?? '';
+                    el.dataset.name         = site.name            ?? '';
+                    el.dataset.campsiteType = site.type            ?? '';
+                    el.dataset.people       = site.max_people      ?? '';
+                    el.dataset.vehicles     = site.max_vehicles    ?? '';
+                    el.dataset.electricity  = site.has_electricity ? 'true' : 'false';
+                    el.dataset.notes        = site.notes           ?? '';
+                    openModal(el);
+                });
                 marker.addTo(groups[type]);
             });
 
-            L.control.layers(null, groups, { collapsed: false }).addTo(map);
+            const layerControl = L.control.layers(null, groups, { collapsed: false, position: 'topleft' }).addTo(map);
+            layerControl.getContainer().style.cssText = `
+                margin-top: 2%;
+                margin-left: 2%;
+                min-width: 115px;
+            `;
 
             const img = new Image();
             img.onload = () => {
